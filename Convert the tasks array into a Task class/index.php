3c0aaa74301c7eddf86f5e4914fa7579b844dad2
Abd-alRahim Kojan <abdalrahimk083@gmail.com>
2026-08-12 11:11:47 +0300

    Convert the tasks array into a Task class

---
+++ Convert the tasks array into a Task class/index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/Task.php';
+$tasks = require_once __DIR__ . '/tasks.php';
+
+$doneTasks = 0;
+
+foreach ($tasks as $task) {
+    echo "{$task->title()}\n";
+    if ($task->isDone()) {
+        $doneTasks++;
+    }
+}
+
+echo "\n{$doneTasks} of " . count($tasks) . " tasks are done.\n";
